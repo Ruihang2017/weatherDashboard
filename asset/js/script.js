a06aab5eddbaf2c66ef929dae718a815bd0bd7f9
Ruihang2017 <94610820+Ruihang2017@users.noreply.github.com>
2023-07-16 22:36:40 +1000

    more weatherAPI

--- asset/js/script.js
+++ asset/js/script.js
@@ -2,10 +2,10 @@ const appid = "0f6b26473e144361f9f4478387b457f2";
 
 
 let currentLocation = document.getElementById("currentLocation");
+let currentDate = document.getElementById("currentDate");
 let currentTemp = document.getElementById("currentTemp");
 let currentWind = document.getElementById("currentWind");
 let currentHumid = document.getElementById("currentHumid");
-
 let forecastDays = document.getElementById("forecastDays");
 
 
@@ -16,6 +16,8 @@ let forecastDays = document.getElementById("forecastDays");
 let weatherData = {
     location: "location",
     currentWeather: {
+        date: "date",
+        icon: "icon",
         temp: "temp",
         wind: "wind",
         humid: "humid"
@@ -24,11 +26,7 @@ let weatherData = {
 };
 
 
-// get the image urls and load to the carousel using unsplash API
 const getWeatherAPI = () => {
-    // let apiUrl = `https://api.unsplash.com/search/photos/?page=${page}&per_page=${per_page}&query=${destination}&client_id=${client_id}`;
-    // let slides = document.getElementsByClassName('carousel-cell');
-    // let titles = document.getElementsByClassName('title');
 
     let lat = 44.34;
     let lon = 10.99;
@@ -39,28 +37,37 @@ const getWeatherAPI = () => {
         .then((response) => {
             if (response.ok) {
                 response.json().then((data) => {
+                    const currentDate = new Date();
                     weatherData.location = data.city.name;
-                    weatherData.currentWeather.temp = `Temp: ${data.list[0].main.temp} &#8451;`;
-                    weatherData.currentWeather.wind = `Wind: ${data.list[0].wind.speed} mph;`;
+                    weatherData.currentWeather.date = `(${currentDate.getDate()}/${currentDate.getMonth()}/${currentDate.getFullYear()})`;
+                    console.log(weatherData.currentWeather.date);
+                    weatherData.currentWeather.temp = `Temp: ${Math.floor(data.list[0].main.temp - 273.15)} &#8451;`;
+                    weatherData.currentWeather.wind = `Wind: ${data.list[0].wind.speed} m/s;`;
                     weatherData.currentWeather.humid = `Humidity: ${data.list[0].main.humidity} %;`;
 
                     console.log(data);
-                    const currentDate = new Date();
 
                     data.list.forEach(element => {
                         let date = new Date(element.dt * 1000)
                         let dateString = `${date.getDate()} - ${date.getMonth()}`;
                         console.log(dateString);
 
+                        // push the forecast data into the array
                         if (currentDate.getDate() !== date.getDate()) {
 
-                            let forecast = {
-                                date: dateString,
-                                temp: `Temp: ${element.main.temp} &#8451;`,
-                                wind: `Wind: ${element.wind.speed} mph;`,
-                                humid: `Humidity: ${element.main.humidity} %;`
+                            let overlap = weatherData.forecastWeather.some(forecast => {
+                                return forecast.date === dateString;
+                            })
+                            if (!overlap) {
+                                let forecast = {
+                                    date: dateString,
+                                    icon: "icon",
+                                    temp: `Temp: ${Math.floor(element.main.temp - 273.15)} &#8451;`,
+                                    wind: `Wind: ${element.wind.speed} m/s;`,
+                                    humid: `Humidity: ${element.main.humidity} %;`
+                                }
+                                weatherData.forecastWeather.push(forecast);
                             }
-                            weatherData.forecastWeather.push(forecast);
                         }
 
                     });
@@ -78,6 +85,7 @@ const getWeatherAPI = () => {
 
 const displayWeatherAPIData = () => {
     currentLocation.innerHTML = weatherData.location;
+    currentDate.innerHTML = weatherData.currentWeather.date;
     currentTemp.innerHTML = weatherData.currentWeather.temp;
     currentWind.innerHTML = weatherData.currentWeather.wind;
     currentHumid.innerHTML = weatherData.currentWeather.humid;
@@ -87,12 +95,13 @@ const displayWeatherAPIData = () => {
             <div class="col-sm-4 col-md-3 col-lg-2">
             <h4>${forecast.date}</h4>
             <i class="bi bi-brightness-high text-warning"></i>
-            <div>Temp</div>
-            <div>Wind</div>
-            <div>Humidity</div>
+            <div>${forecast.temp}</div>
+            <div>${forecast.wind}</div>
+            <div>${forecast.humid}</div>
             </div>`;
     })
 }
+
 
 getWeatherAPI();
 
